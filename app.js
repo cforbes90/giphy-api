8210cdb9c0016795b9d9c21fc98f8df4e7d4f53f
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-01 15:29:18 -0400

    Beginning framework and API call buildout

---
+++ app.js
@@ -0,0 +1,9 @@
+//var topics=[ "its always sunny in philadelphia", "Agents of Shield", "Bill Burr" ]
+var topics =["Joy",
+    "Excitement",
+    "Surprise",
+    "Sadness",
+    "Anger",
+    "Disgust",
+    "Contempt",
+    "Fear"]
