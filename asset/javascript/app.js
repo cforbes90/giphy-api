937c28befd80729c5855f2c09f6c2479758e88d1
shrfu31 <55064372+shrfu31@users.noreply.github.com>
2019-10-02 02:37:55 -0400

    got the added new 10 feature working. Will clean up consoles later

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -2,25 +2,30 @@
 
 
 var gif="";
- 
+var gifArray=[];
 var topics =["Joy","Excitement","Surprise","Sadness","Anger","Disgust","Contempt","Fear"];
-
+var random=0;
   // displaygifInfo function re-renders the HTML to display the appropriate content
 function displayGifInfo() {
-  $("#gif-view").empty();
-
+  //$("#gif-view").empty();
+  
   var query = $(this).attr("data-name");
   //console.log("query is " + query);
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=ttW7fYuN6GCKvPWa0Vn5T0Lox99zjQT2&limit=10&rating=pg-13";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=ttW7fYuN6GCKvPWa0Vn5T0Lox99zjQT2&limit=10&rating=pg-13&offset="+random;
 
   $.ajax({
     url: queryURL,
     method: "GET"
     }).then(function(response) {
-      //console.log(response.data.url);
+      console.log(response.data);
       gif=response.data;
-      //console.log(gif);
-      populateGif();      
+      console.log("random is "+ random);
+        if (random==0){
+          populateGif(); 
+          random+=10;  
+        } else {
+          gifArrayPersistence();   
+        }
     });
 
   }
@@ -42,8 +47,8 @@ function displayGifInfo() {
     //$("#gif-view").empty();
     // Looping through the array of gifs from Response
     for (var i = 0; i < gif.length; i++) {       
-      var gifDiv = $("<div class='gif'>");
-      
+      gifArray.push(gif[i]);
+      var gifDiv = $("<p class='gif'>");
       gifDiv.addClass('gif col-md-4 ');
       //gifDiv.attr("data-name", gif[i]);
       gifDiv.append('<img class="gifStill" src='+gif[i].images.fixed_height_still.url + ' data-still=' + gif[i].images.fixed_height_still.url + ' data-load=' + gif[i].images.fixed_height.url + ' data-state="still"/>');
@@ -79,5 +84,22 @@ function displayGifInfo() {
         $(this).attr('src', $(this).attr('data-still'));
         $(this).data('state', 'still');
       }
-});
+  });
+  function gifArrayPersistence () {
+    random+=10;
+    //for(b=0; b<gif.length; b++)
+    //gifArray.push(gif[b]);
+    for(i=0; i<gifArray.length; i++) {
+      var gifDiv = $("<p class='gif'>");
+      console.log("gifArray length is " +gifArray.length);
+      gifDiv.addClass('gif col-md-4 ');
+      //gifDiv.attr("data-name", gif[i]);
+      gifDiv.prepend('<img class="gifStill" src='+gif[i].images.fixed_height_still.url + ' data-still=' + gif[i].images.fixed_height_still.url + ' data-load=' + gif[i].images.fixed_height.url + ' data-state="still"/>');
+      gifDiv.prepend('<span class="label rated label-default">Rated: '+gif[i].rating.toUpperCase() + '</span>'+'<p></p>');
+      gifDiv.prepend('<span class="label title label-default">Title: '+gif[i].title + '</span>'+'<p></p>');
+      $("#gif-view").prepend(gifDiv);    
+    }
+
+
+  }
 
